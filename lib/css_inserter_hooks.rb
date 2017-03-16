05c891b84294a4c50eb2a97340320a2a5fd10023
Onur Güzel <onur@artistanbul.io>
2017-03-16 23:53:53 +0300

    Fix CSS output

--- lib/css_inserter_hooks.rb
+++ lib/css_inserter_hooks.rb
@@ -2,16 +2,20 @@ require File.dirname(__FILE__) + '/../app/helpers/rpcadmin_helper.rb'
 
 class CssInserterHooks < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context = { })
-        $css_block = '<style type="text/css">' << "\n" << "#admin-menu .priority-colors {background-image: url(/images/changeset.png)} \n";
+        $css_block = '<style type="text/css">' << "\n" << "#admin-menu .redmine-priority-colors {background-image: url(/images/changeset.png)} \n";
 
         if (RpcadminHelper.isPluginEnabled)
             $settingsDisplay = RpcadminHelper.getSettingsDisplay
 
             $settingsDisplay.each_pair do |trSettingKey, value|
-                $tdGenericClass = trSettingKey.split('-')[0]
-                
                 if (!value.nil?)
-                    $css_block = $css_block << "table.list.issues .#{trSettingKey} ." << $tdGenericClass << " { #{value} }\n"
+                    $css_block = $css_block << "table.list.issues tr.#{trSettingKey} {"
+                    value.each {|k, v|
+                        if (!v.to_s.empty?)
+                            $css_block = $css_block << k << ": " << v << ";"
+                        end
+                    }
+                    $css_block = $css_block << "}\n"
                 end
             end
         end
